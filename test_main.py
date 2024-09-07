4065e350915340abf781dbd30cc7c09077d1fb76
31good <zijunw31@gmail.com>
2024-09-06 20:09:30 -0400

    test_main fixed

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ def test_reverse_string():
     """testing reverse_string function"""
     assert reverse_string("ab") == "ba"
     assert reverse_string("abC") == "Cba"              
-    assert reverse_string("abC") == "a" 
-        
+    #assert reverse_string("abC") == "a" 
+
 if __name__ == "__main__":
     test_reverse_string()
